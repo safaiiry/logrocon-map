68abe43c53eaa42767a3d5a460464d6c523d487e
safaiiry <arina.safronova.viantec@gmail.com>
2024-07-17 12:08:27 +0300

    fixed var on const/let

--- js/saveData.js
+++ js/saveData.js
@@ -1,16 +1,15 @@
-var markerData = [];
+let markerData = [];
 
 document.getElementById('saveDataBtn').addEventListener('click', function() {
-    var listItems = document.querySelectorAll('.marker-item');
+    const listItems = document.querySelectorAll('.marker-item');
     
     listItems.forEach(function(item) {
-        var coordinates = item.textContent.split(': ')[1].split(' , ');
-        var lat = parseFloat(coordinates[0]);
-        var lng = parseFloat(coordinates[1]);
+        const coordinates = item.textContent.split(': ')[1].split(' , ');
+        const lat = parseFloat(coordinates[0]);
+        const lng = parseFloat(coordinates[1]);
 
-        var markerObj = { lat: lat, lng: lng };
+        const markerObj = { lat: lat, lng: lng };
         markerData.push(markerObj);
-    })
+    });
     console.log(markerData);
 });
-
--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
+// Определяем карту, координаты центра и начальный масштаб
+const map = L.map('map').setView([50, 90], 3);
+let markers = [];
 
-//Определяем карту, координаты центра и начальный масштаб
-var map = L.map('map').setView([50, 90], 3);
-var markers = [];
- 
-//Добавляем на нашу карту слой OpenStreetMap
+// Добавляем на нашу карту слой OpenStreetMap
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a rel="nofollow" href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 map.on('click', function(e) {
-    var lat = e.latlng.lat;
-    var lng = e.latlng.lng;
+    const lat = e.latlng.lat;
+    const lng = e.latlng.lng;
 
-    let liElement = document.createElement('li');
+    const liElement = document.createElement('li');
     liElement.className = 'marker-item';
 
     // Добавляем новый маркер
-    var marker = L.marker([lat, lng]).addTo(map);
+    const marker = L.marker([lat, lng]).addTo(map);
     markers.push(marker);
 
     // Кнопка удаления 
-    let removeButton = document.createElement('button');
+    const removeButton = document.createElement('button');
     removeButton.className = 'remove-button';
     removeButton.innerHTML = '&times;';
     removeButton.onclick = function() {
@@ -30,43 +29,42 @@ map.on('click', function(e) {
         liElement.remove();
     };
 
-    liElement.innerHTML = `Выбранная точка:  ${lat} , ${lng}`;
-    liElement.setAttribute('data-latlng', lat + ',' + lng);
+    liElement.innerHTML = `Выбранная точка: ${lat} , ${lng}`;
+    liElement.setAttribute('data-latlng', `${lat},${lng}`);
     liElement.marker = marker;
     liElement.appendChild(removeButton);
     document.querySelector('.marker-list').appendChild(liElement);
-})
-
+});
 
 // Центрирование карты по клику на элемент списка
 document.querySelector('.marker-list').addEventListener('click', function(e) {
-    var latlng = e.target.getAttribute('data-latlng').split(',');
-    var lat = parseFloat(latlng[0]);
-    var lng = parseFloat(latlng[1]);
+    const latlng = e.target.getAttribute('data-latlng').split(',');
+    const lat = parseFloat(latlng[0]);
+    const lng = parseFloat(latlng[1]);
     map.setView([lat, lng], 10); 
-})
+});
 
 // Очистка данных 
-document.getElementById('clearDataBtn').addEventListener('click', function(e) {
+document.getElementById('clearDataBtn').addEventListener('click', function() {
     document.querySelector('.marker-list').innerHTML = '';
     markers.forEach(marker => {
         map.removeLayer(marker);
     });
     markers = [];
-})
+});
 
-// Фильтр по координатам (?)
-document.getElementById('filterInput').addEventListener('input', function(e) {
-    var filterValue = document.getElementById('filterInput').value;
-    var markerList = document.querySelector('.marker-list');
-    var markerItems = markerList.querySelectorAll('.marker-item');
+// Фильтр по координатам
+document.getElementById('filterInput').addEventListener('input', function() {
+    const filterValue = document.getElementById('filterInput').value;
+    const markerList = document.querySelector('.marker-list');
+    const markerItems = markerList.querySelectorAll('.marker-item');
 
     markerItems.forEach(item => {
-        var latlng = item.getAttribute('data-latlng');
+        const latlng = item.getAttribute('data-latlng');
         if (latlng.includes(filterValue)) {
             item.style.display = ''; 
         } else {
             item.style.display = 'none'; 
         }
     });
-})
+});
